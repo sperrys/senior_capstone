24d3808ed74d18aa7a6b93dc61503b2de583b20e
Spencer Perry <spencerfarrallperry@gmail.com>
2019-04-06 16:20:18 -0400

    updated udp to jsonify data

--- udp_server.py
+++ udp_server.py
@@ -1,5 +1,6 @@
 import socket
 import requests
+import struct
 import sys
 
 
@@ -7,8 +8,7 @@ SERVER_HOST = ''
 SERVER_PORT = 80
 
 INGRES_ENDPOINT = '/ingress'
-
-
+KEYS = ['AcX', 'AcY', 'AcZ', 'GyX', 'GyY', 'GyZ', 'time']
 
 
 # Create a TCP/IP socket
@@ -22,28 +22,37 @@ print('starting up on {} port {}'.format(server_address, port))
 
 sock.bind(server_address)
 
+def main():
+
+    readings = struct.Struct("<hhhhhhhI")
+
+    while True:
+
+        print('\nWaiting to receive message')
+
+        data, address = sock.recvfrom(17 * 128)
+
+        #print('Received {} bytes from {}'.format(len(data), address))
+        #print(data)
+
+        if data:
+            values = readings.unpack(data)
+            json = dict(zip(KEYS, values))
+            send_to_server(json)
+
+
 
 
 def send_to_server(data):
 
     url = 'https://{}:{}{}'.format(SERVER_HOST, SERVER_PORT, INGRES_ENDPOINT)
-    r = requests.post(url)
+    r = requests.post(url, json=data)
 
     if r.status_code != 200:
         print("ERR! Unsuccessful request: {}\n Data: {}", url, data)
 
 
-while True:
 
-    print('\nWaiting to receive message')
-
-    data, address = sock.recvfrom(4096)
-
-    print('Received {} bytes from {}'.format(len(data), address))
-    print(data)
-
-    if data:
-        send_to_server(data)
 
 
 
